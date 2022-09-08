700652a3c6daf63943646e15fd375aedaa0f3922
billy le <lebilly87@gmail.com>
2022-09-08 11:31:12 -0700

    added real-time active rooms with users count. added button to join active rooms

--- public/lobby.js
+++ public/lobby.js
@@ -3,7 +3,46 @@ const socket = io();
 
 const LOBBY_MESSAGE = "lobby";
 
+const [heroImg, roomsContainer, roomsList] = ["hero-img", "rooms-container", "rooms"].map((id) =>
+  document.querySelector(`#${id}`)
+);
+
 function createRoomList(rooms) {
+  if (roomsList) {
+    roomsList.innerHTML = "";
+    rooms.forEach(([name, users]) => {
+      const listItem = document.createElement("li");
+      listItem.classList.add("room-item");
+
+      const roomName = document.createElement("h4");
+      const content = document.createElement("div");
+      const usersWrapper = document.createElement("div");
+      const usersIcon = document.createElement("i");
+      const usersCount = document.createElement("p");
+      const joinButtonWrapper = document.createElement("div");
+      const joinButton = document.createElement("button");
+
+      roomName.textContent = name;
+      usersCount.textContent = users;
+      joinButton.textContent = "JOIN";
+
+      content.classList.add("room-content");
+      usersWrapper.classList.add("users");
+      usersIcon.classList.add(...["fa-solid", "fa-users"]);
+      joinButtonWrapper.classList.add("join-button");
+      joinButton.classList.add("button");
+
+      joinButton.addEventListener("click", () => {
+        window.location = `/room/${name}`;
+      });
+
+      usersWrapper.append(usersCount, usersIcon);
+      joinButtonWrapper.append(joinButton);
+      content.append(usersWrapper, joinButtonWrapper);
+      listItem.append(roomName, content);
+      roomsList.append(listItem)
+    });
+  }
 }
 
 socket.on(LOBBY_MESSAGE, (data) => {
@@ -14,3 +53,15 @@ socket.on(LOBBY_MESSAGE, (data) => {
       break;
   }
 });
+
+window.addEventListener("resize", () => {
+  roomsContainer.style.height = heroImg.clientHeight + "px";
+});
+
+async function init() {
+  roomsContainer.style.height = heroImg.clientHeight + "px";
+  const rooms = await fetch("/rooms").then((res) => res.json());
+  createRoomList(rooms);
+}
+
+init();
